5522a923ee3b76fa29cc15f12513883e6454a2c4
Alan Morales <alanrmorales@gmail.com>
2018-12-16 17:59:06 -0800

    adding client install files

---
+++ client/install.sh
@@ -0,0 +1,16 @@
+# copy the lines to the tmux config
+cat ./conf/.tmux.conf >> ~/.tmux.conf
+mkdir -p ~/.unicorntmuxpi/
+cp ./conf/set_unicorn.sh ~/.unicorntmuxpi/
+# tmux config might not pick up the focus event setting on
+# the tmux config file, so run the hook command on shell startup
+cmd="tmux set-hook window-pane-changed 'run-shell \"~/.unicorntmuxpi/set_unicorn.sh #{pane_id}\"'"
+if [ -e ~/.bashrc ]
+then
+echo $cmd >> ~/.bashrc
+fi
+
+if [ -e ~/.zshrc ]
+then
+echo $cmd >> ~/.zshrc
+fi
